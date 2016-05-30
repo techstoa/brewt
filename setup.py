1551a4f6e941f1acc2588b258600d353d0aa1eaa
Gregory Boyce <gregory.boyce@gmail.com>
2016-05-30 16:31:22 -0400

    Finish rename

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='brewt',
       author='Gregory Boyce',
       author_email='gregory.boyce@gmail.com',
       url='https://github.com/gdfuego/brewt',
-      py_modules = ['brewt.py', 'brewt_gpg.py'],
+      py_modules = ['brewt.py'],
+      scripts['brewt_gpg'],
       )
