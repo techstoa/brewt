35885e9a8e0cc944a8edb9d0836acec28fa2a581
Gregory Boyce <gregory.boyce@gmail.com>
2016-05-30 12:38:23 -0400

    Add ability to specify max and min word lengths

--- bruteforce.py
+++ bruteforce.py
@@ -9,6 +9,10 @@ def setup():
     parser = argparse.ArgumentParser(description='usage: %prog [options]')
     parser.add_argument('--passfile', '-p', default=False, required=True,
             help="File containing password list.  One per line.")
+    parser.add_argument('--minwords', default=1, type=int,
+            help="Minimum number of words to use in combinations.")
+    parser.add_argument('--maxwords', type=int,
+            help="Maximum number of words to use in combinations.")
     args = parser.parse_args()
     return args
 
@@ -20,9 +24,15 @@ def main():
     for line in f.readlines():
         wordlist.append(line.strip('\n'))
 
+    if options.maxwords:
+        maxwords = options.maxwords
+    else:
+        # Add 1 since the length is 0 indexed
+        maxwords = len(wordlist)
+
     # Cycle through each password, then all permutations of
     # combining two passwords, then 3, etc up to the length of the array.
-    for i in xrange(1,len(wordlist)+1):
+    for i in xrange(options.minwords,maxwords+1):
         for p in permutations(wordlist, i):
             print ''.join(p)
 
