a7afdeab90517a70edee5e5492b4a523336c1cad
Gregory Boyce <gregory.boyce@gmail.com>
2016-05-30 12:12:40 -0400

    Improve formatting and allow filename as argument

--- bruteforce.py
+++ bruteforce.py
@@ -4,8 +4,27 @@ from itertools import permutations
 
 wordlist = []
 
-f = open('test.txt')
-for line in f.readlines():
-    wordlist.append(line.strip('\n'))
+def setup():
+    import argparse
+    parser = argparse.ArgumentParser(description='usage: %prog [options]')
+    parser.add_argument('--passfile', '-p', default=False, required=True,
+            help="File containing password list.  One per line.")
+    args = parser.parse_args()
+    return args
 
-print wordlist
+def main():
+    options = setup()
+
+    # Build an array of all known passwords
+    f = open(options.passfile)
+    for line in f.readlines():
+        wordlist.append(line.strip('\n'))
+
+    # Cycle through each password, then all permutations of
+    # combining two passwords, then 3, etc up to the length of the array.
+    for i in xrange(1,len(wordlist)+1):
+        for p in permutations(wordlist, i):
+            print ''.join(p)
+
+if __name__ == '__main__':
+    main()
