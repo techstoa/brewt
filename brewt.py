9d613d0ef9c17bb1f8d6e48358f7a628196d9639
Gregory Boyce <gregory.boyce@gmail.com>
2016-05-30 19:31:55 -0400

    pep8 cleanup

--- brewt.py
+++ brewt.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python
 
+"""Script for generating password possibilities"""
+
 def setup():
+    """parse arguments"""
     import argparse
     parser = argparse.ArgumentParser(description='usage: %prog [options]')
     parser.add_argument('--passfile', '-p', default=False, required=True,
-            help="File containing password list.  One per line.")
+                        help="File containing password list.  One per line.")
     parser.add_argument('--minwords', default=1, type=int,
-            help="Minimum number of words to use in combinations.")
+                        help="Minimum number of words to use in combinations.")
     parser.add_argument('--maxwords', type=int,
-            help="Maximum number of words to use in combinations.")
+                        help="Maximum number of words to use in combinations.")
     args = parser.parse_args()
     return args
 
 def generate_list(wordlist, min_words, max_words):
-    """Cycle through each password, then all permutations of combining two passwords, 
-    then 3, etc up to the length of the array."""
+    """Cycle through each password, then all permutations of combining two
+    passwords, then 3, etc up to the length of the array."""
     from itertools import permutations
     mylist = []
-    for i in xrange(min_words,max_words):
-        for p in permutations(wordlist, i):
-            mylist.append(''.join(p))
+    for i in xrange(min_words, max_words):
+        for current_option in permutations(wordlist, i):
+            mylist.append(''.join(current_option))
     return mylist
 
 def main():
+    """Non-module logic for running as a commandline tool"""
     options = setup()
 
     wordlist = []
     # Build an array of all known passwords
-    f = open(options.passfile)
-    for line in f.readlines():
+    file_handle = open(options.passfile)
+    for line in file_handle.readlines():
         wordlist.append(line.strip('\n'))
 
     if options.maxwords:
